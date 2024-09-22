b3fa86a8d3fb23592b12b108f3006f7bd2c9984a
Salim Damerdji <31995532+sdamerdji@users.noreply.github.com>
2024-09-22 10:48:48 -0700

    add checkP4, which is for housing plans full in compliance

--- extractpdf.py
+++ extractpdf.py
@@ -34,7 +34,9 @@ class Extract:
     def getPageCount(self):
         self.pageCount = len(self.document)
     def findPhrases(self,phrase):
-        if re.search(r"\b{}\b".format(phrase), self.pdf_text):
+        regex = r"\b{}\b".format(re.sub(r"\s+", r"\\s+", phrase))
+        result = re.search(regex, self.pdf_text)
+        if result:
             return True
         else:
             return False
@@ -45,11 +47,25 @@ def main():
     pdf_dir = './pdfs/'
     df = pd.read_excel('he-review-letters.xlsx', )
     df['REVIEWED'] = pd.to_datetime(df['REVIEWED']).dt.date
-    df1 = df[(df.TYPE == 'INITIAL DRAFT') & (df.REVIEWED > datetime.date(year=2020, month=1, day=1))]
+    df1 = df[(df.TYPE == 'INITIAL DRAFT') & (df.REVIEWED >= datetime.date(year=2019, month=1, day=1))]
+
+    # the first draft is not labelled an initial draft
+    unlabelled_initial_drafts = df[~df.JURISDICTION.isin(df1.JURISDICTION)]
+    unlabelled_initial_drafts = unlabelled_initial_drafts[unlabelled_initial_drafts['TYPE'] == 'DRAFT']
+    unlabelled_initial_drafts = unlabelled_initial_drafts[unlabelled_initial_drafts['PLANNING CYCLE'] == 6]
+
+    # Group by JURISDICTION and return the row with the minimum 'REVIEWED' date
+    unlabelled_initial_drafts = unlabelled_initial_drafts.groupby('JURISDICTION', group_keys=False).apply(
+        lambda g: g.loc[g['REVIEWED'].idxmin()]
+    )
+    df1 = pd.concat((df1, unlabelled_initial_drafts))
+
     df2 = df[(df.TYPE == 'ADOPTED')][['JURISDICTION','REVIEWED']]
     df2 = df2.groupby('JURISDICTION').agg({'REVIEWED':'max'}).reset_index()
 
-    email_dict= {'JURISDICTION':[],'EMAIL':[],'ID_DATE':[],'AD_DATE':[],'COUNTY':[],'pageCount':[],"checkP1":[],'checkP2':[],'checkP3':[]}
+    email_dict= {'JURISDICTION':[],
+                 'EMAIL':[],'ID_DATE':[],'AD_DATE':[],'COUNTY':[],
+                 'pageCount':[],"checkP1":[],'checkP2':[],'checkP3':[], 'checkP4':[]}
     for juris,url,date,county in zip(df1['JURISDICTION'],df1['LINK TO REVIEW LETTER'],df1['REVIEWED'],df1['COUNTY']):
         pdf_path = pdf_dir+ url.split("/")[-1]
 
@@ -62,6 +78,7 @@ def main():
         emails = [email for email in emails if email != 'sitesinventory@hcd.ca.gov']
         adopted_date = df2[df2.JURISDICTION == juris].REVIEWED
         for email in emails:
+
             email_dict['EMAIL'].append(email)
             email_dict['JURISDICTION'].append(juris)
             email_dict['ID_DATE'].append(date)
@@ -71,9 +88,18 @@ def main():
             checkP1 = ex.findPhrases('addresses many statutory requirements')
             checkP2= ex.findPhrases( 'addresses some statutory requirements')
             checkP3 = ex.findPhrases( 'addresses most statutory requirements')
+            checkP4 = ex.findPhrases( 'element in substantial compliance')
+            checkP4 |= ex.findPhrases( 'The housing element will comply with State Housing Element Law')
+            checkP4 |= ex.findPhrases( 'The housing element will comply with housing element law')
+            checkP4 |= ex.findPhrases( 'The housing element will comply with State Housing Element law')
+            checkP4 |= ex.findPhrases(
+                'The housing element will comply when it is adopted')
+            checkP4 |= ex.findPhrases('housing element will comply with the law when it is adopted')
+
             email_dict['checkP1'].append(checkP1)
             email_dict['checkP2'].append(checkP2)
             email_dict['checkP3'].append(checkP3)
+            email_dict['checkP4'].append(checkP4)
 
             if adopted_date.empty:
                 email_dict['AD_DATE'].append(date)
